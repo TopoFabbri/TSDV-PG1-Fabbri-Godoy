68ebad7aeb7678983cc551f2bd90017c3793299d
gytsss <totogodoy@hotmail.com>
2023-09-04 23:24:25 -0300

    Add vertex attributes pointer

--- Engine_Godoy_Fabbri/src/Source/Renderer.cpp
+++ Engine_Godoy_Fabbri/src/Source/Renderer.cpp
@@ -6,14 +6,17 @@ namespace ToToEng
 	{
 		float vertices[9] =
 		{
-			0.f, .5f, 0.f,
-			-.5f, -.5f, 0.f,
+			 0.f, .5f, 0.f,
+		   -.5f, -.5f, 0.f,
 			.5f, -.5f, 0.f
 		};
 
 		glGenBuffers(1, &VBO);
 		glBindBuffer(GL_ARRAY_BUFFER, VBO);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
 
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
 
